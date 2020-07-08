38dee6997aef8173737391baa5e9439ed92c204d
Hypons <hypons@me.com>
2020-07-08 19:53:32 +0800

    add duplicate data checking

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const bodPar = require('body-parser')
 const mongoose = require('mongoose')
 const generator = require('./generator.js')
 const Link = require('./models/link')
-const MONGODB_URI = 'mongodb://localhost/storten-link' || process.env.MONGODB_URI
-const host = "http://localhost:3000/" || "https://conservative-parliament-43220.herokuapp.com/"
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/storten-link'
+const host = "https://conservative-parliament-43220.herokuapp.com/" || "http://localhost:3000/"
 
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -28,41 +28,35 @@ app.get('/', (req, res) => {
 app.post('/url', (req, res) => {
   let originLink = req.body.link
 
+  // prevent empty input 
   if (!originLink) {
     res.render('show')
   } else {
 
-
-    let existedData = ""
-
+    // check if new url already exists in db 
+    let existsData = []
     Link.find({ originLink: originLink })
-      .then(data => existedData = data)
-
-
-    if (existedData === []) {
-      console.log("1", existedData)
-      const code = generator()
-
-
-      return Link.create({ originLink, code })
-        .then(() => Link.find({ "originLink": originLink }))
-        .then(() => res.render('show', { originLink, code, host }))
-        .catch(error => console.log('error'))
-
-    } else {
-      console.log("2", existedData)
-      Link.find({ originLink: originLink })
-        .lean()
-        .then(data => res.render('show', { originLink: data[0].originLink, code: data[0].code, host }))
-    }
+      .then(data => existsData = data)
+      .then(data => {
+        if (existsData.length === 0) {
+          const code = generator()
+          return Link.create({ originLink, code })
+            .then(() => Link.find({ originLink: originLink }))
+            .then(() => res.render('show', { originLink, code, host }))
+            .catch(error => console.log('error'))
+        } else {
+          Link.find({ originLink: originLink })
+            .lean()
+            .then(data => res.render('show', { originLink: data[0].originLink, code: data[0].code, host }))
+        }
+      })
   }
 })
-
 
 app.get('/:code', (req, res) => {
   const code = req.params.code
   Link.find({ code: code })
-    .then(data => res.redirect("https://" + data[0].originLink))
+    .then(data => res.redirect(data[0].originLink))
 })
 
 
--- generator.js
+++ generator.js
@@ -1,11 +1,14 @@
 const Link = require('./models/link')
 
 function generator() {
-  const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  // const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
   let code = ''
 
   while (code.length < 5) {
-    code += digits[Math.floor(Math.random() * digits.length)]
+    code = Math.random().toString(36).slice(-5)
+
+    // code += digits[Math.floor(Math.random() * digits.length)]
   }
 
   let uniqueCode = Link.find({ code: code })
